d6a0def07b1e2e6ed916ba4298ef13888ddf5bcc
mirmina <49362896+mirmina@users.noreply.github.com>
2019-10-07 14:57:43 +0300

    project pertama

--- app/src/main/java/com/example/gykdenemeprojesi/MainActivity.java
+++ app/src/main/java/com/example/gykdenemeprojesi/MainActivity.java
@@ -27,13 +27,11 @@ public class MainActivity extends Activity {
     RadioGroup radioGroup;
     RadioButton radioButton;
     Spinner ay;
-    String aytext;
-    private Object String;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
-  {
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -49,16 +47,17 @@ public class MainActivity extends Activity {
         radioGroup = findViewById(R.id.radiGroup);
 
         bttn = findViewById(R.id.bttn);
-        bttn.setOnClickListener(new View.OnClickListener() {
+        bttn.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v)
+            {
 
                 String b = kilo.getText().toString();
                 String c = boy.getText().toString();
                 String d = bascevresi.getText().toString();
                 int cinsiyet = radioGroup.getCheckedRadioButtonId();
                 radioButton = findViewById(cinsiyet);
-                String str = ay.getSelectedItem().toString();
 
                 if (!b.isEmpty() && !c.isEmpty() && !d.isEmpty() && radioButton.isChecked()) {
 
@@ -67,8 +66,13 @@ public class MainActivity extends Activity {
                     boytext = Double.parseDouble(c);
                     bastext = Double.parseDouble(d);
 
-                    if(radioButton.getText().equals("Kız")){ // kız
-                        if(String == "Yeni Doğan"){ // ay==0
+                    if (radioButton.getText().equals("Kız"))  // kız
+                    {
+                        String str = ay.getSelectedItem().toString();
+
+                        if (str.equals("Yeni Doğan")) ;
+                        {
+                            // ay==0
                             if (bastext >= 31.7 && bastext < 33.9)
                                 text1.setText("Persentil eğrisi %3, baş çevresi gelişimi Zayıf");
                             else if (bastext == 33.9)
@@ -96,8 +100,8 @@ public class MainActivity extends Activity {
                             else
                                 text3.setText("Girilen değer yanlış");
                         }
-
-                        else if(String == "1 Aylık"){ //ay == 1
+                        if (str.equals("1 Aylık")) //ay == 1
+                        {
                             if (bastext >= 34.3 && bastext < 36.5)
                                 text1.setText("Persentil eğrisi %3, baş çevresi gelişimi Zayıf");
                             else if (bastext == 36.5)
@@ -125,74 +129,79 @@ public class MainActivity extends Activity {
                             else
                                 text3.setText("Girilen değer yanlış");
                         }
-
-
                     }
-                    else{ //erkek
 
-                            if (String == "Yeni Doğan") { // ay==0
-                                if (bastext >= 3.1 && bastext < 34.5)
-                                    text1.setText("Persentil eğrisi %3, baş çevresi gelişimi Zayıf");
-                                else if (bastext == 34.5)
-                                    text1.setText("Persentil eğrisi %50, baş çevresi gelişimi Normal");
-                                else if (bastext > 34.5 && bastext <= 36.9)
-                                    text1.setText("Persentil eğrisi %97, baş çevresi gelişimi Yüksek");
-                                else
-                                    text1.setText("Girilen değer yanlış");
 
-                                if (boytext >= 46.3 && boytext < 49.9)
-                                    text2.setText("Persentil eğrisi %3, boy gelişimi Zayıf");
-                                else if (boytext == 49.9)
-                                    text2.setText("Persentil eğrisi %50, boy gelişimi Normal");
-                                else if (boytext > 49.9 && boytext <= 53.4)
-                                    text2.setText("Persentil eğrisi %97, boy gelişimi Yüksek");
-                                else
-                                    text2.setText("Girilen değer yanlış");
+                    else if (radioButton.getText().equals("Erkek")) // Erkek
+                    {
+                        String str = ay.getSelectedItem().toString();
+                        if (str.equals("Yeni Doğan")) // ay==0
+                        {
+                            if (bastext >= 3.1 && bastext < 34.5)
+                                text1.setText("Persentil eğrisi %3, baş çevresi gelişimi Zayıf");
+                            else if (bastext == 34.5)
+                                text1.setText("Persentil eğrisi %50, baş çevresi gelişimi Normal");
+                            else if (bastext > 34.5 && bastext <= 36.9)
+                                text1.setText("Persentil eğrisi %97, baş çevresi gelişimi Yüksek");
+                            else
+                                text1.setText("Girilen değer yanlış");
 
-                                if (kilotext >= 2.5 && kilotext < 3.3)
-                                    text3.setText("Persentil eğrisi %3, kilo gelişimi Zayıf");
-                                else if (kilotext == 3.3)
-                                    text3.setText("Persentil eğrisi %50, kilo gelişimi Normal");
-                                else if (kilotext > 3.3 && kilotext <= 4.3)
-                                    text3.setText("Persentil eğrisi %97, kilo gelişimi Yüksek");
-                                else
-                                    text3.setText("Girilen değer yanlış");
-                            } else if (String == "1 Aylık") { //ay == 1
-                                if (bastext >= 35.1 && bastext < 37.3)
-                                    text1.setText("Persentil eğrisi %3, baş çevresi gelişimi Zayıf");
-                                else if (bastext == 37.3)
-                                    text1.setText("Persentil eğrisi %50, baş çevresi gelişimi Normal");
-                                else if (bastext > 37.3 && bastext <= 39.5)
-                                    text1.setText("Persentil eğrisi %97, baş çevresi gelişimi Yüksek");
-                                else
-                                    text1.setText("Girilen değer yanlış");
+                            if (boytext >= 46.3 && boytext < 49.9)
+                                text2.setText("Persentil eğrisi %3, boy gelişimi Zayıf");
+                            else if (boytext == 49.9)
+                                text2.setText("Persentil eğrisi %50, boy gelişimi Normal");
+                            else if (boytext > 49.9 && boytext <= 53.4)
+                                text2.setText("Persentil eğrisi %97, boy gelişimi Yüksek");
+                            else
+                                text2.setText("Girilen değer yanlış");
 
-                                if (boytext >= 51.1 && boytext < 54.7)
-                                    text2.setText("Persentil eğrisi %3, boy gelişimi Zayıf");
-                                else if (boytext == 54.7)
-                                    text2.setText("Persentil eğrisi %50, boy gelişimi Normal");
-                                else if (boytext > 54.7 && boytext <= 58.4)
-                                    text2.setText("Persentil eğrisi %97, boy gelişimi Yüksek");
-                                else
-                                    text2.setText("Girilen değer yanlış");
+                            if (kilotext >= 2.5 && kilotext < 3.3)
+                                text3.setText("Persentil eğrisi %3, kilo gelişimi Zayıf");
+                            else if (kilotext == 3.3)
+                                text3.setText("Persentil eğrisi %50, kilo gelişimi Normal");
+                            else if (kilotext > 3.3 && kilotext <= 4.3)
+                                text3.setText("Persentil eğrisi %97, kilo gelişimi Yüksek");
+                            else
+                                text3.setText("Girilen değer yanlış");
+                        }
+                        else if ((str.equals("1 Aylık"))) //ay == 1
+                        {
+                            if (bastext >= 35.1 && bastext < 37.3)
+                                text1.setText("Persentil eğrisi %3, baş çevresi gelişimi Zayıf");
+                            else if (bastext == 37.3)
+                                text1.setText("Persentil eğrisi %50, baş çevresi gelişimi Normal");
+                            else if (bastext > 37.3 && bastext <= 39.5)
+                                text1.setText("Persentil eğrisi %97, baş çevresi gelişimi Yüksek");
+                            else
+                                text1.setText("Girilen değer yanlış");
 
-                                if (kilotext >= 3.4 && kilotext < 4.5)
-                                    text3.setText("Persentil eğrisi %3, kilo gelişimi Zayıf");
-                                else if (kilotext == 4.5)
-                                    text3.setText("Persentil eğrisi %50, kilo gelişimi Normal");
-                                else if (kilotext > 4.5 && kilotext <= 5.7)
-                                    text3.setText("Persentil eğrisi %97, kilo gelişimi Yüksek");
-                                else
-                                    text3.setText("Girilen değer yanlış");
+                            if (boytext >= 51.1 && boytext < 54.7)
+                                text2.setText("Persentil eğrisi %3, boy gelişimi Zayıf");
+                            else if (boytext == 54.7)
+                                text2.setText("Persentil eğrisi %50, boy gelişimi Normal");
+                            else if (boytext > 54.7 && boytext <= 58.4)
+                                text2.setText("Persentil eğrisi %97, boy gelişimi Yüksek");
+                            else
+                                text2.setText("Girilen değer yanlış");
 
-                            }
-                        }}
-                    else{
-                            Toast.makeText(getApplicationContext(), "Lütfen tüm boşlukları doldurunuz", Toast.LENGTH_LONG).show();
+                            if (kilotext >= 3.4 && kilotext < 4.5)
+                                text3.setText("Persentil eğrisi %3, kilo gelişimi Zayıf");
+                            else if (kilotext == 4.5)
+                                text3.setText("Persentil eğrisi %50, kilo gelişimi Normal");
+                            else if (kilotext > 4.5 && kilotext <= 5.7)
+                                text3.setText("Persentil eğrisi %97, kilo gelişimi Yüksek");
+                            else
+                                text3.setText("Girilen değer yanlış");
                         }
 
+                    }
 
                 }
-            });
-        }
+                else
+                {
+                    Toast.makeText(getApplicationContext(), "Lütfen tüm boşlukları doldurunuz", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
 }
